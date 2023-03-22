74f6b08348423abe47fd5e8975e1cbd07208efa4
Пётр Румянцев <dogboxer64529000@gmail.com>
2023-03-23 02:58:37 +0300

    fix: Исправил верстку (чуть-чуть)

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -14,11 +14,11 @@ function Login() {
     <h3 className='entry__input-name'>E-mail</h3>
     <input className='entry__input entry__input_type_email' type='email' required></input>
     <h3 className='entry__input-name'>Пароль</h3>
-    <input className='entry__input entry__input_type_password' type='password' required minLength='2' maxLength='30'></input>
+    <input className='entry__input entry__input_type_password entry__input_type_password-change-margin' type='password' required minLength='2' maxLength='30'></input>
     <button className='entry__button-register'>Войти</button>
     <p className='entry__text'>Ещё не зарегистрированы?
      <Link className='entry__text-link' to='signup'>
-      Зарегистрироваться
+      Регистрация
      </Link>
     </p>
    </form>
